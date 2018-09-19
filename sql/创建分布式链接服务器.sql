2dcc3c1d501cde778eb138075a308a782691eb19
leeneo@xingzhihen.com <leeneo@xingzhihen.com>
2018-09-19 18:26:04 +0800

    1

---
+++ sql/创建分布式链接服务器.sql
@@ -0,0 +1,18 @@
+
+--创建链接服务器，远程执行数据库的存储过程
+exec sp_addlinkedserver 'TestServer','','SQLOLEDB','120.76.41.xx'
+exec sp_addlinkedsrvlogin 'TestServer','false',null,'sa','pwd'
+exec sp_serveroption 'TestServer','rpc out','true' --这个允许调用链接服务器上的存储过程
+
+--查询已有链接服务器
+ select * from sys.servers
+
+--调用远程服务器存储过程：
+exec TestServer.数据库名.dbo.过程名 '参数'
+
+--demo:
+select * from TestServer.TestDB.dbo.regcard where s_print_no = '17376852911'
+
+declare @as_error varchar(200)
+exec TestServer.TestDB.dbo.Usp_Insert_cardaccount3 '', '11', '消费', @as_error output
+select @as_error
